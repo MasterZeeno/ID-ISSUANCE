63d6f98bb14855ea30298ac3f03f66fec211b23b
Jay Ar Adlaon Cimacio, RN, DM II <zeenoliev@gmail.com>
2024-10-09 03:24:45 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,12 @@ import { defineConfig } from 'vite'
 
 const srcFolder = resolve(__dirname, 'src')
 const cssFolder = resolve(srcFolder, 'css')
-const jsFolder = resolve(srcFolder, 'js')
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: resolve(jsFolder, 'main.js')
-    }
-  },
   resolve: {
     alias: {
       '@': srcFolder,
-      '@css': cssFolder,
-      '@js': jsFolder
+      '@css': cssFolder
     }
   }
 })
